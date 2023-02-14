be33532cf30f85306599eb4f62394ff41b1ec2ad
anamarijamatevska <anamarija.matevskalola@gmail.com>
2023-02-14 18:57:36 +0100

    Design improvements and logout functionality

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:coffe_shop_app/model/coffee.dart';
 import 'package:coffe_shop_app/screens/cart_screen.dart';
+import 'package:coffe_shop_app/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,19 @@ class _ProfileState extends State<ProfileScreen> {
 
   final List<Coffee> _cartList = <Coffee>[];
   var currentUser = FirebaseAuth.instance.currentUser;
+
+    Future _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut().then((value) {
+        print("User signed out");
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoginScreen()));
+      });
+    } on FirebaseAuthException catch (e) {
+      print("ERROR HERE");
+      print(e.message);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,15 +81,15 @@ class _ProfileState extends State<ProfileScreen> {
         ],
       ),
       body: Column(children: <Widget>[
-        ColoredBox(
-          color:const Color(0xFF7B5B36),
+        const ColoredBox(
+          color:Color(0xFF7B5B36),
           child: SizedBox(
           width: double.infinity,
           height: 50,
           child: Center(
             child: Text(
-            'Welcome back ${currentUser!.email}',
-            style: const TextStyle(
+            'Welcome back!',
+            style: TextStyle(
                 color: Color.fromARGB(255, 237, 234, 231),
                 fontWeight: FontWeight.bold,
                 fontSize: 20),
@@ -90,7 +104,7 @@ class _ProfileState extends State<ProfileScreen> {
           height: 50,
           child: Center(
             child: Text(
-            'Check out your recent orders:',
+            'Here is your profile data:',
             style: TextStyle(
                 color: Color.fromARGB(255, 237, 234, 231),
                 fontWeight: FontWeight.bold,
@@ -100,23 +114,40 @@ class _ProfileState extends State<ProfileScreen> {
           ),
         ),
         ),
-        const SizedBox(
-          height: 30,
-        ),
-        const Text(
-          'Email',
-          style: TextStyle(
-            fontSize: 16
+        Padding(
+          padding: const EdgeInsets.fromLTRB(50, 20, 50, 20),
+          child: Column(children: <Widget>[
+          const Text(
+            'Email',
+            style: TextStyle(
+              fontSize: 16
+            ),
           ),
-        ),
-              TextField(
-                textInputAction: TextInputAction.done,
-                decoration: InputDecoration(
-                  hintText: currentUser!.email.toString(),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0))
-                ),
-                obscureText: true,
-              ),
+          TextField(
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              hintText: currentUser!.email.toString(),
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0))
+            ),
+            obscureText: true,
+          ),
+          const SizedBox(
+            height: 350,
+          ),
+          ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)
+              )
+            ),
+            label: const Text("Log Out", style: TextStyle(fontSize: 24),),
+            icon: const Icon(Icons.logout, size: 25,),
+            onPressed: _signOut,
+            ),    
+          ]
+          ),
+        )    
       ]
       )
     );
