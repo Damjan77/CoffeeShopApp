5e04601e08b0f28ba9a462fc248f7f5f747614b0
Zagorka <jovanovazagorka@outlook.com>
2023-02-19 23:51:05 +0100

    Modified user interface

--- lib/screens/order_successfull_screen.dart
+++ lib/screens/order_successfull_screen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:coffe_shop_app/custom/custom_app_bar.dart';
 import 'package:coffe_shop_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -20,40 +21,51 @@ class _OrderSuccessfullScreenState extends State<OrderSuccessfullScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed:  () {
+          Navigator.of(context).push(
+              MaterialPageRoute(
+              builder: (context) => HomeScreen(),));
+        },
+        backgroundColor: const Color(0xFF7B5B36),
+        label: const Text("HOME"),
+        icon: Icon(Icons.home),
+      ),
+      appBar: CustomAppBar(
+        appBar: AppBar(),
+        widgets: [],
+      ),
       backgroundColor: const Color(0xFFFCF5C9),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+          Container(
+            child: Icon(
+              Icons.celebration_rounded,
+              size: 60,
+              color: Colors.green,
+            ),
+          ),
           Align(
             child: Text(
               'Your order was successful!',
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 25,
+                fontSize: 30,
                 fontWeight: FontWeight.bold,
-                color: Colors.green[400]
+                color: Color(0xFF7B5B36)
               ),
             ), 
           ),
-          SizedBox(height: 100),
-          Align(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(70, 0, 70, 0),
-              child: ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size.fromHeight(50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0)
-                  )
-                ),
-                label: Text("Home", style: TextStyle(fontSize: 24),),
-                icon: Icon(Icons.home, size: 25,),
-                onPressed: redirectHome,
-              ),
-            )
-          )
+          SizedBox(height: 10,),
+          Text("Thank you!",
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF7B5B36)
+            ),)
         ]),
     );
   }
