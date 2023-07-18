cc55a9d07f741c3fdf8be73e047f1a5ef3c2f3d0
Mila Dodevska <dodevskamila@gmail.com>
2023-07-18 23:10:11 +0200

    Added AppBar

--- lib/screens/info_screen.dart
+++ lib/screens/info_screen.dart
@@ -16,29 +16,53 @@ class _InfoState extends State<InfoScreen> {
   MapController mapController = Get.put(MapController());
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     mapController.fetchLocations();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: CustomAppBar(
+        appBar: AppBar(),
+        widgets: [
+          Padding(
+              padding: const EdgeInsets.only(right: 16.0, top: 30.0),
+              child: GestureDetector(
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: <Widget>[
+                    Icon(
+                      Icons.add_business_rounded,
+                      size: 30.0,
+                    ),
+                  ],
+                ),
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ProfileScreen(),
+                    ),
+                  );
+                },
+              ))
+        ],
+      ),
       body: SafeArea(
-        child: Obx(
-            () => mapController.mapModel.isNotEmpty
-                ? GoogleMap(
+          child: Obx(
+        () => mapController.mapModel.isNotEmpty
+            ? GoogleMap(
                 initialCameraPosition: const CameraPosition(
                   target: LatLng(42.00478491557928, 21.40917442067392),
                   zoom: 14.4746,
                 ),
-              markers: mapController.markers,
-            ) : Center(
-              child: CircularProgressIndicator(),
-            ),
-        )
-      ),
+                markers: mapController.markers,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      )),
     );
   }
 }
